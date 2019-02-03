3bb1364452a1a0f520d18b3190d6e099a281dc88
Samuel C <scusi@gruporpp.com.pe>
2019-02-03 18:52:04 -0500

    RUTAS

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import { HashRouter, Match } from 'react-router'
+// Cambiar el import de react-router por este otro
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'normalize-css'
 
 import styles from './app.css'
 import Header from '../Header'
 import Main from '../Main'
+import Profile from '../Profile'
+import Login from '../Login'
 
 class App extends Component {
   constructor(){
@@ -14,16 +17,63 @@ class App extends Component {
         photoURL: 'https://pbs.twimg.com/profile_images/1014574035995381760/xxA7qmVp_bigger.jpg',
         email: 'samurc.net@gmail.com',
         displayName: 'Samuel Cusi',
+        location: 'Lima, Perú'
       }
     }
+
+    this.handleOnAuth = this.handleOnAuth.bind(this)
   }
 
-  render (){
+  handleOnAuth(){
+    console.log('Auth');
+  }
+
+
+  // Modificar el método render con los nuevos componentes: HashRouter por Router,
+  // añadir Switch y cambiar Match por Route y en lugar de pattern utilizar
+  // la propiedad path
+  render () {
     return (
+      <Router>
         <div>
           <Header />
-          <Main user={this.state.user} />
+          <Switch>
+            <Route exact path='/' render={() => {
+              if (this.state.user) {
+                return (
+                  <Main
+                  user={this.state.user}
+                  onLogout={this.handleLogout}
+                  />
+                )
+              } else {
+                return (
+                  <Login onAuth={this.handleOnAuth} />
+                )
+              }
+            }} />
+            <Route path='/profile' render={() => {
+              return (
+                <Profile
+                  picture={this.state.user.photoURL}
+                  username={this.state.user.email.split('@')[0]}
+                  displayName={this.state.user.displayName}
+                  location={this.state.user.location}
+                  emailAddress={this.state.user.email}
+                />
+              )
+            }} />
+            <Route path='/user/:username' render={({ params }) => {
+              return (
+                <Profile
+                  displayName={params.username}
+                  username={params.username}
+                />
+              )
+            }} />
+          </Switch>
         </div>
+      </Router>
     )
   }
 }
